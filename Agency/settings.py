ccfdc0fbdff003ec55f1bbde5bbee91cc0f82bd4
Manoj G Rao <71961842+manojrao3619@users.noreply.github.com>
2021-06-24 20:15:52 +0530

    Update settings.py

--- Agency/settings.py
+++ Agency/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manoja.rao9@gmail.com'
-EMAIL_HOST_PASSWORD = 'maxprince'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
