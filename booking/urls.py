645f51cf243699129ee3aa6b93c0847b86dd8581
Manoj G Rao <71961842+manojrao3619@users.noreply.github.com>
2021-07-24 23:44:20 +0530

    Added Booking History

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url 
-from .views import booking, cancelBooking
+from .views import booking, cancelBooking, bookingHistory
  
 urlpatterns = [ 
     url(r'^booking$', booking),
-    url(r'^cancel$', cancelBooking)
+    url(r'^cancel$', cancelBooking),
+    url(r'^history$', bookingHistory),
     ]
