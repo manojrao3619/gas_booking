cc47e4e12101e4a06995469b629c85d4f3b69e4e
Manoj G Rao <71961842+manojrao3619@users.noreply.github.com>
2021-08-08 23:21:58 +0530

    Update settings.py

--- Agency/settings.py
+++ Agency/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
 
 
